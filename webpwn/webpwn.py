aa60cadf538a656d14f857a1e9564517997bec14
highjack <bsheppard.xiii@gmail.com>
2021-04-03 16:29:45 +0100

    adding early test version of webpwn

---
+++ webpwn/webpwn.py
@@ -0,0 +1,95 @@
+import pyfiglet
+from colorama import init, Fore, Back, Style
+import hashlib
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import os.path
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+# Web Exploitation Framework
+#     coded by highjack
+
+class webpwn:
+    proxy_enabled = False
+    proxies = {'http': 'http://192.168.1.222:31337','https': 'http://192.168.1.222:31337',}
+    debug_enabled = False
+
+    def __init__(self, exploit_name, author, date, proxy_enabled=False, debug_enabled=False):
+        self.exploit_name = exploit_name
+        self.author = author
+        self.date = date
+        self.proxy_enabled = proxy_enabled
+        self.banner(self.exploit_name, self.author, self.date)
+        self.debug_enabled = debug_enabled
+    
+    def error(self, message):
+        print("[❌] {}".format(message))
+        exit()
+    def status(self, message):
+        print("[❕] {}".format(message))
+    def success(self, message):
+        print("[✔️} {}".format(message))
+    def debug(self, message):
+            if self.debug_enabled == True:
+                print("[🐞] {}".format(message))
+
+    def banner(self, exploit_name, author, date):
+        logo = pyfiglet.figlet_format("webpwn", font = "cosmike" )
+        print("{}{}".format(Fore.RED,logo))
+        print("{}[- Exploit -]: {}{}".format(Fore.BLUE,Fore.CYAN,exploit_name))
+        print("{}[- Date -]: {}{}".format(Fore.BLUE, Fore.CYAN, date))
+        print("{}[- Author -]: {}{}{}".format(Fore.BLUE, Fore.CYAN, author, Style.RESET_ALL))
+        self.debug("[+] Proxy Enabled: {}".format(str(self.proxy_enabled)))
+
+   
+    def hash(self, method, input):
+        m = ""
+        if method == "sha512":
+            m = hashlib.sha512()
+        elif method == "sha1":
+            m = hashlib.sha1()
+        elif method == "md5":
+            m = hashlib.md5()
+        elif method == "sha256":
+            m = hashlib.sha256()
+        else:
+            return ""
+        m.update(input.encode('ascii'))
+        return m.hexdigest()
+
+    
+    def request(self, method, url, data=None, session=None, cookies=None, headers=None, useragent=None, redirects=True, filepath=None, file_parameter=None):
+        self.debug("test")
+        method = method.upper()
+        if self.proxy_enabled == False:
+            proxies = None
+        else:
+            proxies = self.proxies
+        if useragent != None:
+            useragent_dict = {"User-Agent":useragent}
+            if headers != None:
+                headers.update(useragent_dict)
+            else:
+                headers= useragent_dict
+
+        if session==None:
+            session = requests.Session()
+
+        if method == "GET":
+            r = session.get(url, cookies=cookies, headers=headers, proxies=proxies, verify=False, allow_redirects=redirects)
+        elif method == "POST":
+            r = session.post(url, data=data, cookies=cookies, headers=headers, proxies=proxies, verify=False, allow_redirects=redirects)
+        elif method == "UPLOAD":
+            if filepath != None and file_parameter != None:
+                if os.path.isfile(filepath):
+                    file_parameter = {file_parameter: filehandle}
+                    r = session.post(url, data=data, cookies=cookies, headers=headers, proxies=proxies, verify=False, allow_redirects=redirects, files = filepath)
+                else:
+                    error("Local file \"{}\" does not exist".format(file_path))
+            else:
+                error("filepath and file_parameter not set")
+
+        response = r.text
+        return response, session
+
